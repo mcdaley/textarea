6ae3ce782bac3323bf3d8c2083d5b4fbedf55ac3
Mike Daley <michaelcdaley@yahoo.com>
2016-11-03 08:47:56 -0700

    Initial commit, basic layout is completed

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,7 @@
+#------------------------------------------------------------------------------
+# app/controllers/tasks_controller.rb
+#------------------------------------------------------------------------------
+class TasksController < ApplicationController
+  def index
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161101205616) do
+ActiveRecord::Schema.define(version: 20161101213922) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "due"
+    t.boolean  "complete"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
